b9d7de844e7c0c8ca801b0721d5a6ce8cb61434d
Paul Hawke <paul.hawke@asynchrony.com>
2013-06-12 13:58:59 -0500

    Implemented complete/incomplete event for credit card entry and hooked up a button to enable/disable accordingly.

--- app/src/com/simplify/android/sdk/MainActivity.java
+++ app/src/com/simplify/android/sdk/MainActivity.java
@@ -2,14 +2,40 @@ package com.simplify.android.sdk;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends Activity
 {
+
+    private CreditCardEditor cardEditor;
+    private Button lookupButton;
+
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+
+        lookupButton = (Button)findViewById(R.id.send_button);
+
+        cardEditor = (CreditCardEditor) findViewById(R.id.credit_card);
+        cardEditor.addEntryCompleteListener(new EntryCompleteListener() {
+            @Override
+            public void entryComplete(View editorView) {
+                lookupButton.setEnabled(true);
+            }
+
+            @Override
+            public void entryIncomplete(View editorView) {
+                lookupButton.setEnabled(false);
+            }
+        });
+    }
+
+    public void lookupToken(View eventSource) {
+        Toast.makeText(this, "Sure thing, boss", 2000).show();
     }
 }
