2278a25aec767fdfb9f6560df72b184cf8d8d77f
Paul Hawke <paul.hawke@gmail.com>
2013-06-29 08:59:04 -0500

    Updated project setup to use relative paths

--- docs/project-setup.sh
+++ docs/project-setup.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-~/android-sdk-macosx/tools/android create lib-project --name SimplifyAndroidSDK --target 1 --path ~/AndroidProjects/SimplifyAndroidSDK/sdk --package com.simplify.android.sdk
+android create lib-project --name SimplifyAndroidSDK --target 1 --path ./sdk --package com.simplify.android.sdk
 
-~/android-sdk-macosx/tools/android create project --target 1 --name SimplifyAndroidSdkExample --path ~/AndroidProjects/SimplifyAndroidSDK/app --activity MainActivity --package com.simplify.android.sdk
+android create project --target 1 --name SimplifyAndroidSdkExample --path ./app --activity MainActivity --package com.simplify.android.sdk
 
-~/android-sdk-macosx/tools/android update project --target 1 --path ~/AndroidProjects/SimplifyAndroidSDK/app --library ~/AndroidProjects/SimplifyAndroidSDK/sdk
+android update project --target 1 --path ./app --library ./sdk
 
-~/android-sdk-macosx/tools/android create test-project -m ~/AndroidProjects/SimplifyAndroidSDK/app -n unittest -p ~/AndroidProjects/SimplifyAndroidSDK/unittest
+android create test-project -m ./app -n unittest -p ./unittest
 
-~/android-sdk-macosx/tools/android create test-project -m ~/AndroidProjects/SimplifyAndroidSDK/app -n integrationtest -p ~/AndroidProjects/SimplifyAndroidSDK/integrationtest
+android create test-project -m ./app -n integrationtest -p ./integrationtest
 
